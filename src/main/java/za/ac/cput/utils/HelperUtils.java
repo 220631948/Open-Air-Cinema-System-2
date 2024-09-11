b095f3f23d98c2b200767e9415f723fe1dfb0aff
Ferdi Davids <220631948@mycput.ac.za>
2024-09-11 10:52:02 +0200

    Util Package with helper methods created.

---
+++ src/main/java/za/ac/cput/utils/HelperUtils.java
@@ -0,0 +1,39 @@
+package za.ac.cput.utils;
+
+import java.util.Date;
+
+public class HelperUtils {
+    public static boolean isNullorEmpty(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+
+    public static boolean isNullorEmpty(Object value) {
+        return value == null;
+    }
+
+    public static boolean isNullorEmpty(int value) {
+        return value == 0;
+    }
+
+    public static boolean isNullorEmpty(double value) {
+        return value == 0.0;
+    }
+
+    public static boolean isNullorEmpty(Date value) {
+        return value == null;
+    }
+
+    // is valid payment amount
+    public boolean isValidPaymentAmount(double paymentAmount) {
+        return paymentAmount > 0;
+    }
+
+
+
+
+
+
+
+
+
+}
