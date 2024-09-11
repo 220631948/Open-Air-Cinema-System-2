e935791e4189a58e76099222cae5f54dd7836abc
Ferdi Davids <220631948@mycput.ac.za>
2024-09-11 16:12:10 +0200

    Imported Bootstrap and fontawesome icons for styling into the main

--- cinema-system/src/main.js
+++ cinema-system/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 const app = createApp(App);
 
 app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
