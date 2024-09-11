e921a5a52162a144fc2f02f3ec0c1902ae80cda4
Ferdi Davids <220631948@mycput.ac.za>
2024-09-11 11:26:54 +0200

    TicketRepository class finished.

--- src/main/java/za/ac/cput/repository/TicketRepository.java
+++ src/main/java/za/ac/cput/repository/TicketRepository.java
@@ -1,4 +1,21 @@
 package za.ac.cput.repository;
 
-public interface TicketRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Ticket;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    //find tickets by booking ID
+    List<Ticket> findByBooking_BookingID(int bookingID);
+
+    //find all tickets by seat number
+    List<Ticket> findBySeatNumber(String seatNumber);
+
+    // find tickets by booking ID and seat number
+    List<Ticket> findByBooking_BookingIDAAndSeatNumber(int bookingID, String seatNumber);
+
+
 }
